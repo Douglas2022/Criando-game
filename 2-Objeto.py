74d58e1d0ffa49eec76b3686184428166952ddfe
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-31 10:52:53 -0300

    Intanciando classes com jogos

--- 2-Objeto.py
+++ 2-Objeto.py
@@ -11,6 +11,29 @@ game1.yearLauch = 2017
 game1.multiplayear = False
 game1.note = 9.5
 
+# 2- Segundo jogo
+game2 = Game()
+game2.name = "Fort-Nite"
+game2.yearLauch = 2017
+game2.multiplayear = True
+game2.note = 8.0
 
-print("###Dados do Jogo###")
-print(f"Nome do jogo: {game1.name}\nAno de laçamento: {game1. yearLauch}")
+# 2-Terceiro jogo
+game3 = Game()
+game3.name = " Residente Evil 50"
+game3.yearLauch = 2018
+game3.multiplayear = True
+game3.note = 9,9
+
+# 2-Terceiro jogo
+game4 = Game()
+game4.name = "Soul Heavy"
+game4.yearLauch = 2019
+game4.multiplayear = False
+game4.note = 7.5
+
+print("\n###Dados do Jogo###")
+print(f"\nNome do jogo: {game1.name}\nAno de laçamento: {game1. yearLauch}")
+print(f"\nNome do jogo: {game2.name}\nAno de laçamento: {game2. yearLauch}")
+print(f"\nNome do jogo: {game3.name}\nAno de laçamento: {game3. yearLauch}")
+print(f"\nNome do jogo: {game4.name}\nAno de laçamento: {game4. yearLauch}")
