491ed4fcf1f30baddfe0a27e628d29b53106aeab
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-31 10:39:15 -0300

    Criando primeiro jogo

---
+++ 2-Objeto.py
@@ -0,0 +1,16 @@
+class Game:
+    name = ""
+    yearLauch = 0
+    multiplayear = False
+    note = 0        
+
+# 1- Primeiro jogo
+game1 = Game()
+game1.name = "Legends of Zelda: Breath of the wild"
+game1.yearLauch = 2017
+game1.multiplayear = False
+game1.note = 9.5
+
+
+print("###Dados do Jogo###")
+print(f"Nome do jogo: {game1.name}\nAno de laçamento: {game1. yearLauch}")
