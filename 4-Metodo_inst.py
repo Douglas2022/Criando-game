39e581660b0d8cf7d4b5c0f754ba4c022085b49b
Doouglas Silva <paulodnunes2016@gmail.com>
2026-01-02 08:52:33 -0300

    Metodos de intância em python em segundo metódo

--- 4-Metodo_inst.py
+++ 4-Metodo_inst.py
@@ -4,19 +4,33 @@ class Game:
         self.yearLaunch = yearLaunch
         self.multiplayer = multiplayer
         self.note = note
+        self.totalAvaluation = 0
+        self.evaluator = 0
     
     def __str__(self):
         return f"Game: {self.name}"
     
     def tecnical_sheet(self):
         print("\n##Dados do Jogo##")
-        print(f"O nome do jogo: {self.name}")
+        print(f"\nO nome do jogo: {self.name}")
         print(f"O ano de lançamento: {self.yearLaunch}")
         print(f"Modo multplayear?: {self.multiplayer}")
         print(f"Avaliação do jogo: {self.note}\n")
 
+    def evaluate(self,note):
+        self.totalAvaluation += note
+        self.evaluator += 1
+
+    def everede(self):
+        print(f"A média do filme: {self.name} : {self.totalAvaluation}/ {self.evaluate}")
+
+
 game1 = Game("Legends of strone",2017,False,9.5)
 game2 = Game("Legends of Black",2019,True,8.0)
+
+game1.evaluate(9.0)
+game1.evaluate(7.5)
+game1.everede()
 
 game1.tecnical_sheet()
 game2.tecnical_sheet()
