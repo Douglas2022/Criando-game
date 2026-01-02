3c6e4119d492a337c39789bfeb97f725b2951263
Doouglas Silva <paulodnunes2016@gmail.com>
2026-01-02 09:01:51 -0300

    Metodos em modo antigo

--- 4-Metodo_inst.py
+++ 4-Metodo_inst.py
@@ -22,18 +22,22 @@ class Game:
         self.evaluator += 1
 
     def everede(self):
-        print(f"A média do filme: {self.name} : {self.totalAvaluation}/ {self.evaluate}")
+        print(f"A média do filme: {self.name} : {self.totalAvaluation}/ {self.evaluator}")
 
 
 game1 = Game("Legends of strone",2017,False,9.5)
 game2 = Game("Legends of Black",2019,True,8.0)
 
+game1.tecnical_sheet()
+game2.tecnical_sheet()
 game1.evaluate(9.0)
 game1.evaluate(7.5)
 game1.everede()
+game2.evaluate(7.0)
+game2.evaluate(6.0)
+game2.everede()
 
-game1.tecnical_sheet()
-game2.tecnical_sheet()
+
        
 
     
