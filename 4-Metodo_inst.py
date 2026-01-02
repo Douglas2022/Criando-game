ca04b9af4d2d2bc22cfed502176a8959ae3512fa
Doouglas Silva <paulodnunes2016@gmail.com>
2026-01-02 09:32:46 -0300

    Metodos em modo atualizado

--- 4-Metodo_inst.py
+++ 4-Metodo_inst.py
@@ -1,54 +1,44 @@
 class Game:
-    def __init__(self,name="",yearLaunch=0,multiplayer=0,note=0):
-        self.name = name
-        self.yearLaunch = yearLaunch
+    def __init__(self, nome="", lancamento=0, multiplayer=False, nota_inicial=0):
+        self.nome = nome
+        self.lancamento = lancamento
         self.multiplayer = multiplayer
-        self.note = note
-        self.totalAvaluation = 0
-        self.evaluator = 0
-    
+        self.nota_inicial = nota_inicial
+        self.totalNota = 0
+        self.qtdAvaliacoes = 0
+
     def __str__(self):
-        return f"Game: {self.name}"
+        return f"Game: {self.nome}"
     
-    def tecnical_sheet(self):
-        print("\n##Dados do Jogo##")
-        print(f"\nO nome do jogo: {self.name}")
-        print(f"O ano de lançamento: {self.yearLaunch}")
-        print(f"Modo multplayear?: {self.multiplayer}")
-        print(f"Avaliação do jogo: {self.note}\n")
+    def ficha_tecnica(self):
+        print("\n## Dados do jogo ##")
+        print(f"O nome do jogo: {self.nome}")
+        print(f"O ano de lançamento: {self.lancamento}")
+        print(f"Modo multiplayer: {self.multiplayer}")
+        print(f"Avaliação inicial: {self.nota_inicial}")
 
-    def evaluate(self,note):
-        self.totalAvaluation += note
-        self.evaluator += 1
+    def avaliacao(self, nota):
+        self.totalNota += nota
+        self.qtdAvaliacoes += 1
 
-    def everede(self):
-        print(f"A média do filme: {self.name} : {self.totalAvaluation}/ {self.evaluator}")
-
-
-game1 = Game("Legends of strone",2017,False,9.5)
-game2 = Game("Legends of Black",2019,True,8.0)
-
-game1.tecnical_sheet()
-game2.tecnical_sheet()
-game1.evaluate(9.0)
-game1.evaluate(7.5)
-game1.everede()
-game2.evaluate(7.0)
-game2.evaluate(6.0)
-game2.everede()
+    def media(self):
+        if self.qtdAvaliacoes > 0:
+            media = self.totalNota / self.qtdAvaliacoes
+            print(f"A média do jogo {self.nome}: {media:.2f}")
+        else:
+            print(f"O jogo {self.nome} não possui avaliações")
 
 
-       
+game1 = Game("Legends of Stone", 2017, False, 9.5)
+game2 = Game("Legends of Black", 2019, True, 8.0)
 
-    
+game1.ficha_tecnica()
+game2.ficha_tecnica()
 
+game1.avaliacao(9.0)
+game1.avaliacao(7.5)
+game1.media()
 
-
-
-
-
-
-
-
-
-        
+game2.avaliacao(7.0)
+game2.avaliacao(6.0)
+game2.media()
