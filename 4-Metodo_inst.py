3eee9526e98294b291394587ccce1ce67315966b
Doouglas Silva <paulodnunes2016@gmail.com>
2026-01-05 11:39:46 -0300

    Métodos de instancia

--- 4-Metodo_inst.py
+++ 4-Metodo_inst.py
@@ -1,44 +1,47 @@
 class Game:
-    def __init__(self, nome="", lancamento=0, multiplayer=False, nota_inicial=0):
-        self.nome = nome
-        self.lancamento = lancamento
-        self.multiplayer = multiplayer
-        self.nota_inicial = nota_inicial
-        self.totalNota = 0
-        self.qtdAvaliacoes = 0
+    def __init__(self,Name = "",YearLauch = 0,Multiplayer = 0,Note = 0):
+        self.Name = Name
+        self.YearLAuch = YearLauch
+        self.Multiplayer = Multiplayer
+        self.Note = Note
+        self.TotalAvaluete = 0
+        self.evaluators = 0
 
     def __str__(self):
-        return f"Game: {self.nome}"
+        return f"Game: {self.Name}"
     
-    def ficha_tecnica(self):
-        print("\n## Dados do jogo ##")
-        print(f"O nome do jogo: {self.nome}")
-        print(f"O ano de lançamento: {self.lancamento}")
-        print(f"Modo multiplayer: {self.multiplayer}")
-        print(f"Avaliação inicial: {self.nota_inicial}")
+    def tecnical_sheet(self):
+        print(f"###Dados do jogo###")
+        print(f"Nome do jogo: {self.Name}")
+        print(f"Ano de lançamento: {self.YearLAuch}")
+        print(f"Modo multiplayers: {self.Multiplayer}")
+        print(f"Avaliação do jogo: {self.Name}\n")
 
-    def avaliacao(self, nota):
-        self.totalNota += nota
-        self.qtdAvaliacoes += 1
+    def Avaluete(self,note):
+        self.TotalAvaluete += note
+        self.evaluators += 1
 
-    def media(self):
-        if self.qtdAvaliacoes > 0:
-            media = self.totalNota / self.qtdAvaliacoes
-            print(f"A média do jogo {self.nome}: {media:.2f}")
-        else:
-            print(f"O jogo {self.nome} não possui avaliações")
+    def Averege(self):
+        print(f"Media do filme: {self.Name}: {self.TotalAvaluete / self.evaluators}")
+
+Game1 = Game("Game of Strones",2017,False,9.2)
+Game2 = Game("Game of Zelda",2017,True,8.0)
+
+Game1.tecnical_sheet()
+Game2.tecnical_sheet()
+Game1.Avaluete(9.5)
+Game1.Avaluete(7.0)
+Game1.Averege()
 
 
-game1 = Game("Legends of Stone", 2017, False, 9.5)
-game2 = Game("Legends of Black", 2019, True, 8.0)
 
-game1.ficha_tecnica()
-game2.ficha_tecnica()
 
-game1.avaliacao(9.0)
-game1.avaliacao(7.5)
-game1.media()
 
-game2.avaliacao(7.0)
-game2.avaliacao(6.0)
-game2.media()
+
+
+
+
+
+
+
+        
