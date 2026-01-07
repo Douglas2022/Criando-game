bdd1b219f7a156d753699be5a8a8cc3d11a066f3
Doouglas Silva <paulodnunes2016@gmail.com>
2026-01-07 20:42:49 -0300

    Ultiliando metodos atualizados

--- modelo/biblioteca.py
+++ modelo/biblioteca.py
@@ -1,24 +1,22 @@
-class biblioteca:
+# Metodo de classe
+class Biblioteca():
     bibliotecas = []
+
     def __init__(self,nome):
         self.nome = nome
         self.ativo = False
-        biblioteca.bibliotecas.append(self)
+        Biblioteca.bibliotecas.append(self)
 
     def __str__(self):
         return self.nome
     
-biblioteca_cidade = biblioteca("Biblioteca da cidade")
-biblioteca_shoping = biblioteca("Biblioeca do shoping")
+    def listar_biblioteca():
+        for biblioteca in Biblioteca.bibliotecas:
+            print(f"{biblioteca.nome } ! {biblioteca.ativo}")
+
+biblioteca_cidade = Biblioteca("Biblioteca da cidade")
+biblioteca_shoping = Biblioteca("Biblioteca do shoping")
 print(biblioteca_cidade)
 print(biblioteca_shoping)
 
-
-        
-
-
-
-
-
-
-
+Biblioteca.listar_biblioteca()
