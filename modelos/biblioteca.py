e267e36ca9ce4f056fbd38487cbd5df44ba86e7b
Doouglas Silva <paulodnunes2016@gmail.com>
2026-01-23 10:28:31 -0300

    Metods de classe atualizado

--- modelos/biblioteca.py
+++ modelos/biblioteca.py
@@ -8,9 +8,11 @@ class Biblioteca:
     def __str__(self):
         return self.nome
     
-    def listar_biblioteca():
+    @classmethod
+    def listar_biblioteca(cls):
+        print(f"{'Nome da biblioteca'.ljust(25)} | {'Status'}")
         for biblioteca in Biblioteca.bibliotecas:
-            print(f"{biblioteca.nome} | {biblioteca.ativo}")
+            print(f"{biblioteca.nome.ljust(25)} | {biblioteca.ativo}")
 
     def alternar_estado(self):
         self._ativo = not self._ativo
@@ -26,8 +28,9 @@ biblioteca_shoping = Biblioteca("Biblioteca do shoping")
 biblioteca_cidade.alternar_estado()
 biblioteca_shoping.alternar_estado()
 
+biblioteca_cidade.listar_biblioteca()
 
-Biblioteca.listar_biblioteca()
+
 
 
 
